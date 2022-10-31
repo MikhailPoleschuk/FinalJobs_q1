2859a720474a071043441b76cee17d6b49d79b36
mikle <mr.mikle@mail.ru>
2022-10-31 09:44:49 +0300

    created code

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Код решения
+
+string[] str = new string[] { "1234", "qwe", "rewrt", "fdsef", "gfd", "0q", "1" };
+Console.WriteLine($"Исходный массив строк: {string.Join(" | ", str)}");
+int count = 0;
+for (int i = 0; i < str.Length; i++)
+{
+    if (str[i].Length <= 3) count++;
+}
+
+string[] newstr = new string[count];
+int j = 0;
+for (int i = 0; i < str.Length; i++)
+{
+    if (str[i].Length <= 3)
+    {
+        newstr[j] = str[i];
+        j++;
+    }
+}
+
+Console.WriteLine($"Новый массив строк: {string.Join(" | ", newstr)}");
+
